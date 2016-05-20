c6a73b746049fd83c37660839e1543cc84574b3c
TechBug2012 <mattbstanciu@gmail.com>
2016-05-20 19:43:41 -0400

    init

---
+++ src/SwearToggle.java
@@ -0,0 +1,10 @@
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by TechBug2012 on 5/20/16.
+ */
+public class SwearToggle extends JavaPlugin implements Listener {
+    
+
+}
