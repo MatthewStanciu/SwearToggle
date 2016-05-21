6fbbc8a1fbf5d9d6b1aabc4a6971442546c4808c
TechBug2012 <mattbstanciu@gmail.com>
2016-05-20 22:08:02 -0400

    Trying out a new method. Incomplete atm

--- src/net/extrillius/sweartoggle/SwearToggle.java
+++ src/net/extrillius/sweartoggle/SwearToggle.java
@@ -9,7 +9,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import sun.org.mozilla.javascript.internal.ast.CatchClause;
 
 import java.util.*;
 
@@ -42,20 +41,30 @@ public class SwearToggle extends JavaPlugin implements Listener {
     public void onChat(AsyncPlayerChatEvent event) {
         Set<Player> filteredPlayers = new HashSet<>();
         String newMessage = event.getMessage();
+        String[] splitMessage = event.getMessage().split(" ");
         StringBuilder sb = new StringBuilder();
 
         for (String word : wordList) {
-            if (StringUtils.containsIgnoreCase(newMessage, word)) {
-                for (String pardoned : pardonedList) {
-                    if (word.equalsIgnoreCase(pardoned)) {
-                        break;
+            for (String pardoned : pardonedList) {
+                if (StringUtils.containsIgnoreCase(newMessage, word)) {
+                    for (int i = 0; i < word.length(); i++) {
+                        sb.append("*");
                     }
-                    else {
-                        for (int i = 0; i < word.length(); i++) {
-                            sb.append("*");
+                    newMessage = newMessage.replaceAll(word, sb.toString());
+                    /*
+                    for (String split : splitMessage) {
+                        if (split.equalsIgnoreCase(word)) {
+                            split.replace(split, sb.toString());
                         }
-                        newMessage = newMessage.replaceAll(word, sb.toString());
-                    }
+                        if (split.equalsIgnoreCase(pardoned)) {
+                            split.replace(split, split);
+                        }
+
+                        So my idea here is that I split the message and check each word individually. If that word is a
+                        banned word and not a pardoned word, then censor it. If it is both a banned word AND a pardoned
+                        word, don't do anything -- break, return, whatever. As each word is finished, put it into a
+                        StringBuilder, then set newMessage to sb.toString().
+                    }*/
                 }
             }
         }
@@ -87,45 +96,37 @@ public class SwearToggle extends JavaPlugin implements Listener {
                     saveConfig();
                     reloadConfig();
                     sender.sendMessage(ChatColor.GREEN + "Swearing is now filtered.");
-                }
-                else {
+                } else {
                     sender.sendMessage(ChatColor.RED + "Swearing is already filltered!");
                     return false;
                 }
-            }
-            else if (args[0].equalsIgnoreCase("off")) {
+            } else if (args[0].equalsIgnoreCase("off")) {
                 if (playerList.contains(sender.getName())) {
                     playerList.remove(sender.getName());
                     getConfig().set("players", playerList);
                     saveConfig();
                     reloadConfig();
                     sender.sendMessage(ChatColor.GREEN + "Swearing is now unfiltered.");
-                }
-                else {
+                } else {
                     sender.sendMessage(ChatColor.RED + "Swearing is already unfiltered!");
                     return false;
                 }
-            }
-            else if (args[0].equalsIgnoreCase("reload")) {
+            } else if (args[0].equalsIgnoreCase("reload")) {
                 if (!(sender.hasPermission("swear.reload"))) {
                     sender.sendMessage(ChatColor.RED + "Reloading the plugin is an admin-only command.");
                     return false;
-                }
-                else {
+                } else {
                     reloadConfig();
                     saveConfig();
                     sender.sendMessage(ChatColor.GREEN + "Config reloaded.");
                 }
-            }
-            else if (args[0].equalsIgnoreCase("add")) {
+            } else if (args[0].equalsIgnoreCase("add")) {
                 sender.sendMessage(ChatColor.RED + "Specify a word to add.");
                 return false;
-            }
-            else if (args[0].equalsIgnoreCase("remove")) {
+            } else if (args[0].equalsIgnoreCase("remove")) {
                 sender.sendMessage(ChatColor.RED + "Specify a word to remove.");
                 return false;
-            }
-            else {
+            } else {
                 sender.sendMessage(ChatColor.GREEN + "Commands:");
                 sender.sendMessage(ChatColor.GRAY + "/swear on: " + ChatColor.DARK_GRAY + "Filters swear words.");
                 sender.sendMessage(ChatColor.GRAY + "/swear off: " + ChatColor.DARK_GRAY + "Unfilters swear words.");
@@ -147,8 +148,7 @@ public class SwearToggle extends JavaPlugin implements Listener {
                 if (!(sender.hasPermission("swear.add"))) {
                     sender.sendMessage(ChatColor.RED + "Adding and removing words are admin-only commands!");
                     return false;
-                }
-                else {
+                } else {
                     if (!(wordList.contains(args[1]))) {
                         wordList.add(args[1]);
                         getConfig().set("words", wordList);
@@ -156,19 +156,16 @@ public class SwearToggle extends JavaPlugin implements Listener {
                         reloadConfig();
                         sender.sendMessage(ChatColor.DARK_AQUA + args[1] +
                                 ChatColor.GREEN + " has been added to the list of banned words.");
-                    }
-                    else {
+                    } else {
                         sender.sendMessage(ChatColor.RED + "That word has already been added!");
                         return false;
                     }
                 }
-            }
-            else if (args[0].equalsIgnoreCase("remove")) {
+            } else if (args[0].equalsIgnoreCase("remove")) {
                 if (!(sender.hasPermission("swear.remove"))) {
                     sender.sendMessage(ChatColor.RED + "Adding and removing words are admin-only commands!");
                     return false;
-                }
-                else {
+                } else {
                     if (wordList.contains(args[1])) {
                         wordList.remove(args[1]);
                         getConfig().set("words", wordList);
@@ -176,19 +173,16 @@ public class SwearToggle extends JavaPlugin implements Listener {
                         reloadConfig();
                         sender.sendMessage(ChatColor.DARK_AQUA + args[1] +
                                 ChatColor.GREEN + " has been removed from the list of banned words.");
-                    }
-                    else {
+                    } else {
                         sender.sendMessage(ChatColor.GREEN + "That word is not on the list!");
                         return false;
                     }
                 }
-            }
-            else if (args[0].equalsIgnoreCase("pardon")) {
+            } else if (args[0].equalsIgnoreCase("pardon")) {
                 if (!(sender.hasPermission("swear.pardon"))) {
                     sender.sendMessage(ChatColor.RED + "Pardoning and unpardoningwords are admin-only commands!");
                     return false;
-                }
-                else {
+                } else {
                     if (!(pardonedList.contains(args[1]))) {
                         pardonedList.add(args[1]);
                         getConfig().set("pardoned", pardonedList);
@@ -196,19 +190,16 @@ public class SwearToggle extends JavaPlugin implements Listener {
                         reloadConfig();
                         sender.sendMessage(ChatColor.DARK_AQUA + args[1] +
                                 ChatColor.GREEN + " will not be censored.");
-                    }
-                    else {
+                    } else {
                         sender.sendMessage(ChatColor.RED + "That word is already pardoned!");
                         return false;
                     }
                 }
-            }
-            else if (args[0].equalsIgnoreCase("unpardon")) {
+            } else if (args[0].equalsIgnoreCase("unpardon")) {
                 if (!(sender.hasPermission("swear.unpardon"))) {
                     sender.sendMessage(ChatColor.RED + "Pardoning and unpardoning words are admin-only commands!");
                     return false;
-                }
-                else {
+                } else {
                     if (pardonedList.contains(args[1])) {
                         pardonedList.remove(args[1]);
                         getConfig().set("pardoned", pardonedList);
@@ -218,8 +209,7 @@ public class SwearToggle extends JavaPlugin implements Listener {
                                 ChatColor.GREEN + " will now be censored.");
                     }
                 }
-            }
-            else {
+            } else {
                 sender.sendMessage(ChatColor.RED + "Invalid parameters!");
                 sender.sendMessage(ChatColor.GREEN + "Usage:");
                 sender.sendMessage(ChatColor.GRAY + "/swear add [word]");
